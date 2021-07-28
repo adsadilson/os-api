173216b342bc801623b7880166512d5f957145c6
adsadilson <adilson.curso@yahoo.com.br>
2021-07-28 16:19:38 -0300

    Criação do perfil dev e popular tabelas

--- src/main/java/br/com/apssystem/os/core/config/DBService.java
+++ src/main/java/br/com/apssystem/os/core/config/DBService.java
@@ -9,13 +9,18 @@ import br.com.apssystem.os.domain.entity.OS;
 import br.com.apssystem.os.domain.entity.Tecnico;
 import br.com.apssystem.os.domain.enums.Prioridade;
 import br.com.apssystem.os.domain.enums.Status;
+import br.com.apssystem.os.domain.repository.ClienteRepository;
+import br.com.apssystem.os.domain.repository.OSRepository;
+import br.com.apssystem.os.domain.repository.TecnicoRepository;
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 public class DBService {
 
-	
+	private ClienteRepository clienteRepository;
+	private TecnicoRepository tecnicoRepository;
+	private OSRepository osRepository;
 
 	// @formatter:off
 
@@ -31,6 +36,7 @@ public class DBService {
 		tec.setNome("ADILSON P S. SANTOS");
 		tec.setCpf("339.241.570-73");
 		tec.setTelefone("77 3431-1011");
+		tecnicoRepository.save(tec);
 	}
 	
 	private void popularCliente() {
@@ -38,15 +44,24 @@ public class DBService {
 		cli.setNome("BETINA GONÇALVES");
 		cli.setCpf("338.034.520-23");
 		cli.setTelefone("77 3421-1001");
+		clienteRepository.save(cli);
 	}
 	
 	private void popularOS() {
+		Cliente cli = new Cliente();
+		cli.setId(1L);
+		Tecnico tec = new Tecnico();
+		tec.setId(1L);
+		
 		OS os = new OS();
 		os.setDataAbertura(LocalDate.now());
 		os.setDataFechamento(LocalDate.of(2021, 12, 30));
 		os.setStatus(Status.ATIVO);
 		os.setPrioridade(Prioridade.MEDIA);
 		os.setObservacoes("REPARO NA PLACA MAE DO NOTEBOOK");
+		os.setCliente(cli);
+		os.setTecnico(tec);
+		osRepository.save(os);
 	}
 
 	
